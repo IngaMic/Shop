9981215bb49312d15b8270172b133a0a31ad6f5e
Inga Michneviciute <ingeramich@gmail.com>
2021-04-11 18:23:28 +0200

    custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
 
 import productRoutes from "./routes/productRoutes.js";
 
@@ -15,6 +14,22 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 app.use("/api/products", productRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error(`Not found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        //only need stack for a development mode:
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(
