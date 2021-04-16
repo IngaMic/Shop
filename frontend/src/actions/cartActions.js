16f7f73d46e82ff96d163ef017f793f91ce6f141
Inga Michneviciute <ingeramich@gmail.com>
2021-04-16 21:55:52 +0200

    cart actions and reducers

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+//thunk
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({
+        type: CART_ADD_ITEM,
+        pauload: {
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty,
+        },
+    });
+
+    localStorage.setItem(
+        "cartItems",
+        JSON.stringify(getState().cart.cartItems)
+    );
+};
